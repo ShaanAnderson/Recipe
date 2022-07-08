c88baf9e39494f0ee3e001bb48c0d9abf533c923
ShaanAnderson <108892452+ShaanAnderson@users.noreply.github.com>
2022-07-08 14:03:09 -0500

    Update project.py

--- project.py
+++ project.py
@@ -4,3 +4,47 @@ x=input('What food do you want to make: ')
 if(x=='Cereal'):
   print('1. Poor the cereal, 2. Put your toppings, 3. Poor your milk')
   
+if(x=='Egg'):
+  print('1. For fried eggs, put oil and crack a egg on the pan and cook it for 4 minutes on medium heat. 2. For boiled eggs, fill a pot with water and cook the egg for 6 minutes on high heat, let in set for 10 minutes and crack the shell. 3. For scrambled eggs, wisk the egg with milk and than pour it on a pan with oil, cook it until there is no more liquid and its fluffy on medium-low heat.')
+  
+if(x=='Pancakes'):
+  print('1. ') 
+    
+if(x=='French Toast'):
+  print('1. ') 
+    
+if(x=='Burgers'):
+  print('1. ')
+  
+if(x=='Hot Dog'):
+  print('1. ')
+    
+if(x=='Taco'):
+  print('1. ')
+     
+if(x=='Burrito'):
+  print('1. ')
+     
+if(x=='Steak'):
+  print('1. ')
+  
+if(x=='Pork'):
+  print('1. ')
+  
+if(x=='Chicken'):
+  print('1. ')
+  
+if(x=='Salmon'):
+  print('1. ')
+  
+if(x=='Fried Rice'):
+  print('1. ')
+  
+if(x=='Pasta'):
+  print('1. ')
+  
+if(x=='Soup'):
+  print('1. ')
+  
+if(x=='Pizza'):
+  print('1. ')
