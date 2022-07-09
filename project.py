c3bd78f0559cf84e1ca8d4c082913c7faa01178f
ShaanAnderson <108892452+ShaanAnderson@users.noreply.github.com>
2022-07-09 17:14:52 -0500

    Update project.py

--- project.py
+++ project.py
@@ -23,28 +23,19 @@ if(x=='Taco'):
   print('1. In 10-inch nonstick skillet, cook beef over medium-high heat 5 to 7 minutes, stirring frequently, until thoroughly cooked; drain. 2. Stir in water and taco seasoning mix; heat to boiling. Reduce heat; simmer uncovered 3 to 4 minutes, stirring frequently, until thickened. 3. Divide warmed beef mixture among heated taco shells. Top with cheese, lettuce, avocado, pico de gallo and taco sauce. Top with crema.')
      
 if(x=='Burrito'):
-  print('1. ')
+  print('1. Heat oil in a skillet over high heat. Add garlic and onion, cook for 2 minutes until onion is lightly browned. Add beef and cook, breaking it up as you go, until it turns from red to brown. 2. Add Taco Seasoning (or use 1 packet store bought) and cook for 1 minute. Then add tomato paste and water, cook for 3 minutes until the water is mostly evaporated but the beef is still juicy, not dried out. Set aside to cool for 10 minutes – piping hot filling will steam the burrito, making the tortilla soggy. 3. Optional: If not warming rolled burrito, warm the tortillas before rolling (per packet directions). 4. Place a burrito on a work surface. Place 1/4 - 1/3 cup of rice just below the centre. Top with 1/4 - 1/3 cup beef, then some lettuce, corn, black beans, tomato, a sprinkle of red onion, coriander and cheese. 5. Fold up the bottom to cover the filling then fold the edges in (video helpful to see how to do this). Roll up tightly, then wrap in foil. Serve as is or heat per below. 6. HEATING: To warm through, pan fry (dry pan) in the foil over medium high heat for a few minutes on each side to slightly crisp and warm the outside. Or for 10+ minutes on a medium low heat if you want to melt the cheese. Or they can be baked at 180C/350F for 20 minutes in the foil – this will warm them all the way through and crisp up the tortilla slightly. If they are straight from the fridge, they will take around 30 minutes to warm through.')
      
 if(x=='Steak'):
-  print('1. ')
-  
-if(x=='Pork'):
-  print('1. ')
+  print('1. Pat dry and season the steak generously with salt and pepper – this helps form that amazing crust we all know and love about great steaks. 2. Get your skillet SMOKING HOT before putting the steak in – again, for the crust. 3. WARNING: The butter will sputter when you add the thyme, so stand back! 4. Cook for 6 minutes to get medium rare while flipping every minute. 5. REST your steak for 5 to 10 minutes so it sucks its own juices back in and the fibres relax. This is a must-do step for any protein you cook hard and fast!')
   
 if(x=='Chicken'):
-  print('1. ')
+  print('1. Flatten the Chicken. 2. Season it. 3. Preheat the pan with oil. 4. Add buter to the pan and let it cook till its golden brown.')
   
 if(x=='Salmon'):
-  print('1. ')
-  
-if(x=='Fried Rice'):
-  print('1. ')
+  print('1. Warm a large nonstick skillet with oil over medium-low heat. 2. Season the fish with salt and pepper. 3. Raise the heat to medium-high. Place the salmon, skin-side up in the pan. Cook until golden brown on 1 side, about 4 minutes. Turn the fish over with a spatula, and cook until it feels firm to the touch and the skin is crisp if desired, about 3 minutes more. ')
   
 if(x=='Pasta'):
-  print('1. ')
-  
-if(x=='Soup'):
-  print('1. ')
-  
+  print('1. Boil water. 2. Add salt and than the pasta. 3. Stir. 4. Taste the pasta. 5. Drain the pasta. 6. Add your sauce and meat.')
+
 if(x=='Pizza'):
-  print('1. ')
+  print('1. Roll out the dough and make a circle. 2. Add the sauce. 3. Add the cheese. 4. Add the toppings. 5. Cook the pizza for 10-15 min a 450°F.')
