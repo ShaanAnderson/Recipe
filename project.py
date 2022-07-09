769ba035c6968166b0f91eb5f0905f1ba135d9ff
ShaanAnderson <108892452+ShaanAnderson@users.noreply.github.com>
2022-07-09 16:35:47 -0500

    Update project.py

--- project.py
+++ project.py
@@ -1,5 +1,5 @@
 
-x=input('What food do you want to make: ')
+x=input('What food do you want to make (use capitals): ')
 
 if(x=='Cereal'):
   print('1. Poor the cereal, 2. Put your toppings, 3. Poor your milk')
@@ -8,19 +8,19 @@ if(x=='Egg'):
   print('1. For fried eggs, put oil and crack a egg on the pan and cook it for 4 minutes on medium heat. 2. For boiled eggs, fill a pot with water and cook the egg for 6 minutes on high heat, let in set for 10 minutes and crack the shell. 3. For scrambled eggs, wisk the egg with milk and than pour it on a pan with oil, cook it until there is no more liquid and its fluffy on medium-low heat.')
   
 if(x=='Pancakes'):
-  print('1. ') 
+  print('1. Whisk the eggs and milk together to combine. Add in the flour, and beat until the batter is smooth and lump free. 2. Heat a nonstick pan over low-medium heat with cooking oil spray.') 
     
 if(x=='French Toast'):
-  print('1. ') 
+  print('1. Stir in milk. 2. Dip bread in egg mixture, turning to coat both sides evenly. 3. Cook bread slices on lightly greased nonstick griddle or skillet on medium heat until browned on both sides.') 
     
 if(x=='Burgers'):
-  print('1. ')
+  print('1. Warm the pan. 2. Toast the buns. 3. Increase the heat to medium-high. 4. Cook the burgers for 3 to 5 minutes. 5. Flip the burgers and cook another 3 to 5 minutes.')
   
 if(x=='Hot Dog'):
-  print('1. ')
+  print('1. Bring a small pot of water to a boil. 2. Boil uncovered for 4 to 6 minutes, until the hot dog has plumped up on all sides.')
     
 if(x=='Taco'):
-  print('1. ')
+  print('1. In 10-inch nonstick skillet, cook beef over medium-high heat 5 to 7 minutes, stirring frequently, until thoroughly cooked; drain. 2. Stir in water and taco seasoning mix; heat to boiling. Reduce heat; simmer uncovered 3 to 4 minutes, stirring frequently, until thickened. 3. Divide warmed beef mixture among heated taco shells. Top with cheese, lettuce, avocado, pico de gallo and taco sauce. Top with crema.')
      
 if(x=='Burrito'):
   print('1. ')
